8815302c6fb39b519c7c06ccb5142721e4ab6894
Afsar Uddin <afsaruddin@gmail.com>
2014-02-20 10:15:09 +0600

    Script for maven-jee7 project creation is updated to have user supplied data

--- devel/java/maven-create-jee-project.sh
+++ devel/java/maven-create-jee-project.sh
@@ -1,13 +1,23 @@
 # Check for latest archetype version at: http://mvnrepository.com/artifact/org.codehaus.mojo.archetypes/webapp-javaee7
+
+read -p "Project Name: " project_name
+if [ -z "${project_name}" ]; then exit 1; fi
+
+read -p "Package (e.g., org.something.project): " package
+if [ -z "${package}" ]; then exit 1; fi
+
+read -p "Group Id: " group_id
+if [ -z "${group_id}" ]; then exit 1; fi
+
 mvn \
 	-DarchetypeGroupId=org.codehaus.mojo.archetypes \
 	-DarchetypeArtifactId=webapp-javaee7 \
 	-DarchetypeVersion=1.1 \
 	-DarchetypeRepository=https://nexus.codehaus.org/content/repositories/releases/ \
-	-DgroupId=com.ws.quiztimate \
-	-DartifactId=quiztimate \
+	-DgroupId=${group_id} \
+	-DartifactId=${project_name} \
 	-Dversion=0.1 \
-	-Dpackage=com.ws.quiztimate \
+	-Dpackage=${package} \
 	-Darchetype.interactive=false \
 	--batch-mode \
 	--update-snapshots \
